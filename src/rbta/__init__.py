2d08b93273193d8e41f0dc72c38ff22d2f850f82
arya <aryadharmawm22@gmail.com>
2026-08-28 21:24:25 +0800

    feat: implement single bucket rbta engine

--- src/rbta/__init__.py
+++ src/rbta/__init__.py
@@ -6,11 +6,14 @@ Authoritative implementation of:
 - Single-bucket deterministic RBTA engine
 """
 
+from src.rbta.engine import RBTAEngine, RBTAInvariantError
 from src.rbta.reorder_buffer import LosslessReorderBuffer
 from src.rbta.temporal_state import AgentTemporalState, TemporalStateError
 
 __all__ = [
     "AgentTemporalState",
     "LosslessReorderBuffer",
+    "RBTAEngine",
+    "RBTAInvariantError",
     "TemporalStateError",
 ]
